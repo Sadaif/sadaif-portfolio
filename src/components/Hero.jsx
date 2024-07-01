0d35a19b6083ef75d30370e4b9c93ea997bf1c5a
sadaif <sadaif.lahore@gmail.com>
2024-07-01 22:27:07 +0500

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
               className="bg-gradient-to-r from-pink-300 via-slate-500
 to-purple-500 bg-clip-text lg:text-4xl text-2xl tracking-tight text-transparent"
             >
-              Senior Software Engineer
+              Software Engineer
             </motion.span>
 
             <motion.div
